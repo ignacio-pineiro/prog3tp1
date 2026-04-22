49063dd5aec0b99321d4b8beed9ad3a001eb7e50
Joaquin-CasVar <j.castellanovarela@itecriocuarto.org.ar>
2026-04-22 12:34:06 -0300

    agregado estilo al navbar y footer y texto al index

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,13 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Aplicación para organizar partidos de fútbol
+            Organiza tus partidos de fútbol ⚽
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Anota resultados, jugadores y equipos
+          <p className="max-w-md text-xl leading-8 text-zinc-600 dark:text-zinc-400">
+            Nuestra aplicación te ayuda a planificar tus partidos
+          </p>
+          <p className="max-w-md text-base leading-8 text-zinc-600 dark:text-zinc-400">
+            Anota resultados, agrega jugadores y crea equipos
           </p>
         </div>
       </main>
